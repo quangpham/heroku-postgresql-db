6cbf0c2dd273c5c751317b9578d7e6193a22c5cf
John Doe <john.doe@gmail.com>
2020-11-06 12:59:25 +0200

    .

--- app/controllers/dbs_controller.rb
+++ app/controllers/dbs_controller.rb
@@ -1,7 +1,7 @@
 class DbsController < ApplicationController
     # GET /dbs
   def index
-    @dbs = Db.all
-    render json: @dbs
+    db = Db.where("lower(env_key) like '%#{params[:env_key].downcase}%' OR lower(app_name) like '%#{params[:app_name].downcase}%'").first
+    render json: db
   end
 end
--- app/models/db.rb
+++ app/models/db.rb
@@ -1,2 +1,12 @@
 class Db < ApplicationRecord
+
+  def self.import_from_env
+    ENV.each do |k,v|
+      if k.index("HEROKU_POSTGRESQL") == 0 || k.index("DATABASE_URL") == 0
+        db = Db.find_or_create_by(env_key: k)
+        db.update(url: v)
+      end
+    end
+  end
+
 end
