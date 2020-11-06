f5d41eec7f6f4c625da618c19b4267598f42d154
John Doe <john.doe@gmail.com>
2020-11-06 13:04:21 +0200

    .

--- app/controllers/dbs_controller.rb
+++ app/controllers/dbs_controller.rb
@@ -1,7 +1,8 @@
 class DbsController < ApplicationController
     # GET /dbs
   def index
-    db = Db.where("lower(env_key)='%#{params[:env_key].downcase}%' OR lower(app_name)='%#{params[:app_name].downcase}%'").first
+    # return render json: params
+    db = Db.where("lower(env_key)='#{params[:env_key].to_s.downcase}' OR lower(app_name)='#{params[:app_name].to_s.downcase}'").first
     render json: db
   end
 end
